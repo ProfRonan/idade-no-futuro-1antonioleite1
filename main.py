d4c09cca3c62ed9afae49b59c9177d7b1fa4b626
1antonioleite1 <128155482+1antonioleite1@users.noreply.github.com>
2023-03-29 08:39:14 -0300

    resolvido

---
+++ main.py
@@ -0,0 +1,14 @@
+AnoAtual = int(input("digite o ano atual: "))
+
+Idade = int(input("digite a sua idade: "))
+
+OutroAno = int(input("digite outro ano: "))
+
+Nome = input("Digite seu nome: ")
+
+AnoQueNasceu = AnoAtual - Idade
+
+Resultado = OutroAno - AnoQueNasceu
+
+
+print(f'{Nome}, no ano de {OutroAno} você terá {Resultado} anos')
